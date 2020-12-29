2c849ca4a55d51cd2cf7ec6e1f6f7fab6ae0dd5b
Ryan Gess <ryan.gess@yahoo.com>
2020-12-29 14:28:56 -0500

    Updates

---
+++ precache-manifest.a085ec3715396dd4fd7057a340be43db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52acb422cd290d09d6b0506c2f959abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa35f64e119b883bc2a2",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "1ffa0257255ee774ba95",
+    "url": "/static/css/main.dacc5ac9.chunk.css"
+  },
+  {
+    "revision": "aa35f64e119b883bc2a2",
+    "url": "/static/js/2.f8b01215.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f8b01215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffa0257255ee774ba95",
+    "url": "/static/js/main.eddd4066.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "52a70b08da34c46ddbb2b5143729c4c6",
+    "url": "/static/media/aiguilledumidi.52a70b08.JPG"
+  },
+  {
+    "revision": "42b03ffdc906fce6633f4ff2d40f7870",
+    "url": "/static/media/arl_data_pipeline.42b03ffd.png"
+  },
+  {
+    "revision": "03abd82364762a3350234ce14596d88a",
+    "url": "/static/media/minitab_image.03abd823.png"
+  },
+  {
+    "revision": "8e53a004b17ba9197c951d9a6f8273df",
+    "url": "/static/media/parthean_community.8e53a004.png"
+  }
+]);
